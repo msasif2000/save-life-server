ee85ef888c8c9f78df995073300fdbcc65fa0b1f
Asif <msasifiiuc@gmail.com>
2023-11-28 16:03:41 +0600

    jwt setup done, admin verification done with jwt

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('SAVE LIFE Medical Camp!');
+    res.send('SAVE LIFE MEDICAL CAMP!');
 });
 
 console.log(process.env.DB_User);
@@ -34,26 +35,69 @@ async function run() {
         const userCollection = client.db("SaveLifeDB").collection("users");
         const reviewCollection = client.db("SaveLifeDB").collection("reviews");
 
-        //user collection
-        app.post('/users', async(req, res) => {
+
+        //jwt api
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
+
+        //middlewares
+        const verifyToken = (req, res, next) => {
+            console.log('verify token', req.headers.authorization);
+            if(!req.headers.authorization){
+                return res.status(401).send('Unauthorized request');
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if(err){
+                    return res.status(401).send('Unauthorized request');
+                }
+                req.decoded = decoded;
+                next();
+            })
+            //next();
+        }
+
+        app.get('/user/admin/:email',verifyToken,  async(req, res)=> {
+            const email = req.params.email;
+            if(email!==req.decoded.email){
+                return res.status(403).send('Unauthorized request');
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const admin =false;
+            if(user){
+                admin=user?.role==='admin';
+            }
+            res.send({admin});
+        })
+
+        //user collection
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
             const existingUser = await userCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: 'User already exists'});
+            if (existingUser) {
+                return res.send({ message: 'User already exists' });
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
 
-        app.get('/users', async(req, res) => {
+        app.get('/users',verifyToken, async (req, res) => {
+            //console.log(req.headers);
             const result = await userCollection.find({}).toArray();
             res.send(result);
         })
 
-        app.delete('/users/:id', async(req, res) => {
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
@@ -97,16 +141,16 @@ async function run() {
 
         //make admin
 
-        app.patch('/users/admin/:id', async(req, res) => {
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
-           const updatedDoc = {
-            $set: {
-                role: 'admin'
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
             }
-           }
-           const result = await userCollection.updateOne(filter, updatedDoc);
-           res.send(result);
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
         })
 
         //user access api
